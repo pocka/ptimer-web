a744e0e78086245ca93538782f0eb4e0411d930a
Shota FUJI <pockawoooh@gmail.com>
2024-06-19 17:06:50 +0900

    Load SQLite Worker module
    In order to display non-blank page as soon as possible, I changed a regular import
    to dynamic import.

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,6 +2,35 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-import { Elm } from "./Main.elm";
+interface ElmApp {
+	ports: {};
+}
 
-Elm.Main.init();
+async function loadElm(): Promise<{ Main: { init(): ElmApp } }> {
+	const { Elm } = await import("./Main.elm");
+
+	return Elm;
+}
+
+async function loadSQLite(): Promise<unknown> {
+	const { sqlite3Worker1Promiser } = await import("@sqlite.org/sqlite-wasm");
+
+	return new Promise(resolve => {
+		const promiser = sqlite3Worker1Promiser({
+			onready() {
+				resolve(promiser);
+			},
+		});
+	});
+}
+
+async function main() {
+	const [Elm, sqlite] = await Promise.all([
+		loadElm(),
+		loadSQLite(),
+	]);
+
+	const app = Elm.Main.init();
+}
+
+main();
