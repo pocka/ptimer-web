edd34e041a2b7c07174e0e2b9334d123463512f2
Shota FUJI <pockawoooh@gmail.com>
2024-06-21 20:08:06 +0900

    web: Splash screen foundation

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,6 +2,20 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+const SPLASH_MIN_DURATION_MS = 800;
+
+/**
+ * Displaying splash screen for very short time could make a user thinks the app
+ * is glitching. Non-disturbing minimal artificial wait is necessary.
+ */
+function splashMinDuration(): Promise<void> {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve();
+		}, SPLASH_MIN_DURATION_MS);
+	});
+}
+
 interface ElmApp {
 	ports: {};
 }
@@ -24,10 +38,33 @@ async function loadSQLite(): Promise<unknown> {
 	});
 }
 
+async function runTask<T>(task: Promise<T>, statusID: string): Promise<T> {
+	const el = document.getElementById(statusID);
+	if (!el) {
+		return Promise.reject(new Error(`Splash screen is not loaded: element with ID=${statusID} does not exist`));
+	}
+
+	el.textContent = "...";
+
+	try {
+		const value = await task;
+
+		el.textContent = "OK";
+
+		return value;
+	} catch (error) {
+		console.error(error);
+		el.textContent = "NG";
+
+		// TODO: Display error details
+	}
+}
+
 async function main() {
-	const [Elm, sqlite] = await Promise.all([
-		loadElm(),
-		loadSQLite(),
+	const [, Elm, sqlite] = await Promise.all([
+		splashMinDuration(),
+		runTask(loadElm(), "splash_core"),
+		runTask(loadSQLite(), "splash_db"),
 	]);
 
 	const app = Elm.Main.init();
