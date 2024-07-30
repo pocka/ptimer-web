b4ad8f5d0fe7e0aab619ee9ea8d9de00ed5ac629
Shota FUJI <pockawoooh@gmail.com>
2024-07-30 21:54:27 +0900

    macos: Steps playback
    This is "it works" state.
    
    * No sounds playback.
    * I'm not sure objects, especially scene/state related ones are
    	collectable by GC.
    * Layout is broken. Probably I have to implement Auto Layout.
    * Countdown label does not get show how many seconds left.

--- macos/main.go
+++ macos/main.go
@@ -19,15 +19,29 @@ import (
 	_ "modernc.org/sqlite"
 )
 
+type scene interface {
+	Terminate()
+}
+
+func handleFilePickError(err error) {
+	if _, ok := err.(*filePickCancellation); ok {
+		return
+	}
+
+	alert := appkit.NewAlert()
+	alert.SetMessageText(fmt.Sprintf("Unable to read ptimer file\n%s", err))
+	_ = alert.RunModal()
+}
+
 func main() {
 	macos.RunApp(func(app appkit.Application, delegate *appkit.ApplicationDelegate) {
 		app.SetActivationPolicy(appkit.ApplicationActivationPolicyRegular)
 
-		var window appkit.Window
+		var scene scene
 
-		window = showPickerWindow(func(ptimer *Ptimer) {
-			window.Close()
-			window = showPlayerWindow(ptimer)
+		scene = startPickerScene(func(ptimer *Ptimer) {
+			scene.Terminate()
+			scene = startPlayerScene(ptimer)
 		})
 
 		menu := appkit.NewMenu()
@@ -43,8 +57,8 @@ func main() {
 				return
 			}
 
-			window.Close()
-			window = showPlayerWindow(ptimer)
+			scene.Terminate()
+			scene = startPlayerScene(ptimer)
 		}))
 
 		appMenu.AddItem(appkit.NewMenuItemWithAction("Quit", "q", func(sender objc.Object) { app.Terminate(nil) }))
@@ -59,17 +73,187 @@ func main() {
 	})
 }
 
-func handleFilePickError(err error) {
-	if _, ok := err.(*filePickCancellation); ok {
+type waitingStart struct{}
+
+type playingStep struct {
+	index uint
+}
+
+type allStepsDone struct{}
+
+type playerScene struct {
+	ptimer *Ptimer
+
+	window appkit.Window
+
+	state interface{}
+
+	view appkit.IView
+}
+
+func (p *playerScene) Terminate() {
+	p.window.Close()
+}
+
+func (p *playerScene) Render() {
+	var newView appkit.IView
+
+	switch state := p.state.(type) {
+	case waitingStart:
+		title := appkit.TextField_WrappingLabelWithString(p.ptimer.Metadata.Title)
+		title.SetControlSize(appkit.ControlSizeLarge)
+		title.SetFont(appkit.Font_BoldSystemFontOfSize(16))
+
+		view := appkit.StackView_StackViewWithViews([]appkit.IView{title})
+		view.SetOrientation(appkit.UserInterfaceLayoutOrientationVertical)
+		view.SetDistribution(appkit.StackViewDistributionFill)
+		view.SetAlignment(appkit.LayoutAttributeLeading)
+		view.SetSpacing(8)
+
+		if p.ptimer.Metadata.Description != nil {
+			description := appkit.TextField_WrappingLabelWithString(*p.ptimer.Metadata.Description)
+			view.AddArrangedSubview(description)
+		}
+
+		padding := appkit.NewView()
+		view.AddArrangedSubview(padding)
+
+		next := appkit.NewButtonWithTitle("Start")
+		next.SetKeyEquivalent("\r")
+		next.SetControlSize(appkit.ControlSizeLarge)
+		action.Set(next, func(sender objc.Object) {
+			if len(p.ptimer.Steps) > 0 {
+				p.state = playingStep{index: 0}
+			} else {
+				p.state = allStepsDone{}
+			}
+
+			p.Render()
+		})
+
+		actions := appkit.StackView_StackViewWithViews([]appkit.IView{next})
+		actions.SetOrientation(appkit.UserInterfaceLayoutOrientationHorizontal)
+		actions.SetAlignment(appkit.LayoutAttributeTrailing)
+		view.AddArrangedSubview(actions)
+
+		newView = view
+	case playingStep:
+		children := make([]appkit.IView, 0, 3)
+
+		if state.index < uint(len(p.ptimer.Steps)) {
+			step := p.ptimer.Steps[state.index]
+
+			title := appkit.TextField_WrappingLabelWithString(step.Title)
+			title.SetControlSize(appkit.ControlSizeLarge)
+			title.SetFont(appkit.Font_BoldSystemFontOfSize(16))
+
+			children = append(children, title)
+
+			if step.Description != nil {
+				description := appkit.TextField_WrappingLabelWithString(*step.Description)
+				children = append(children, description)
+			}
+
+			var actionsChildren appkit.IView
+
+			if step.DurationSeconds == nil {
+				next := appkit.NewButtonWithTitle("Next")
+				next.SetKeyEquivalent("\r")
+				next.SetControlSize(appkit.ControlSizeLarge)
+				action.Set(next, func(sender objc.Object) {
+					nextIndex := state.index + 1
+
+					if nextIndex >= uint(len(p.ptimer.Steps)) {
+						p.state = allStepsDone{}
+					} else {
+						p.state = playingStep{index: nextIndex}
+					}
+
+					p.Render()
+				})
+
+				actionsChildren = next
+			} else {
+				next := appkit.TextField_WrappingLabelWithString(fmt.Sprintf("Wait for %d seconds", *step.DurationSeconds))
+
+				timer := foundation.Timer_ScheduledTimerWithTimeIntervalRepeatsBlock(
+					foundation.TimeInterval(*step.DurationSeconds),
+					false,
+					func(timer foundation.Timer) {
+						nextIndex := state.index + 1
+
+						if nextIndex >= uint(len(p.ptimer.Steps)) {
+							p.state = allStepsDone{}
+						} else {
+							p.state = playingStep{index: nextIndex}
+						}
+
+						p.Render()
+					},
+				)
+				objc.Retain(&timer)
+
+				actionsChildren = next
+			}
+
+			actions := appkit.StackView_StackViewWithViews([]appkit.IView{actionsChildren})
+			actions.SetOrientation(appkit.UserInterfaceLayoutOrientationHorizontal)
+			actions.SetAlignment(appkit.LayoutAttributeTrailing)
+
+			children = append(children, actions)
+		} else {
+			text := appkit.TextField_WrappingLabelWithString("Illegal step reached")
+			children = append(children, text)
+		}
+
+		view := appkit.StackView_StackViewWithViews(children)
+		view.SetOrientation(appkit.UserInterfaceLayoutOrientationVertical)
+		view.SetDistribution(appkit.StackViewDistributionFill)
+		view.SetAlignment(appkit.LayoutAttributeLeading)
+		view.SetSpacing(8)
+
+		newView = view
+	case allStepsDone:
+		title := appkit.TextField_WrappingLabelWithString("Completed")
+		title.SetControlSize(appkit.ControlSizeLarge)
+		title.SetFont(appkit.Font_BoldSystemFontOfSize(16))
+
+		next := appkit.NewButtonWithTitle("Back to start")
+		next.SetKeyEquivalent("\r")
+		next.SetControlSize(appkit.ControlSizeLarge)
+		action.Set(next, func(sender objc.Object) {
+			p.state = waitingStart{}
+			p.Render()
+		})
+
+		actions := appkit.StackView_StackViewWithViews([]appkit.IView{next})
+		actions.SetOrientation(appkit.UserInterfaceLayoutOrientationHorizontal)
+		actions.SetAlignment(appkit.LayoutAttributeTrailing)
+
+		view := appkit.StackView_StackViewWithViews([]appkit.IView{title, actions})
+		view.SetOrientation(appkit.UserInterfaceLayoutOrientationVertical)
+		view.SetDistribution(appkit.StackViewDistributionFill)
+		view.SetAlignment(appkit.LayoutAttributeLeading)
+		view.SetSpacing(8)
+
+		newView = view
+	}
+
+	if newView == nil {
 		return
 	}
 
-	alert := appkit.NewAlert()
-	alert.SetMessageText(fmt.Sprintf("Unable to read ptimer file\n%s", err))
-	_ = alert.RunModal()
+	if p.view != nil {
+		p.window.ContentView().ReplaceSubviewWith(p.view, newView)
+	} else {
+		p.window.ContentView().AddSubview(newView)
+	}
+
+	p.view = newView
+	layout.PinEdgesToSuperView(newView, foundation.EdgeInsets{Top: 8, Bottom: 8, Left: 16, Right: 16})
 }
 
-func showPlayerWindow(ptimer *Ptimer) appkit.Window {
+func startPlayerScene(ptimer *Ptimer) *playerScene {
 	window := appkit.NewWindowWithSizeAndStyle(
 		320,
 		160,
@@ -81,44 +265,28 @@ func showPlayerWindow(ptimer *Ptimer) appkit.Window {
 	objc.Retain(&window)
 	window.SetTitle(fmt.Sprintf("%s - Ptimer", ptimer.Metadata.Title))
 
-	title := appkit.TextField_WrappingLabelWithString(ptimer.Metadata.Title)
-	title.SetControlSize(appkit.ControlSizeLarge)
-	title.SetFont(appkit.Font_BoldSystemFontOfSize(16))
-
-	view := appkit.StackView_StackViewWithViews([]appkit.IView{title})
-	view.SetOrientation(appkit.UserInterfaceLayoutOrientationVertical)
-	view.SetDistribution(appkit.StackViewDistributionFill)
-	view.SetAlignment(appkit.LayoutAttributeLeading)
-	view.SetSpacing(8)
-
-	if ptimer.Metadata.Description != nil {
-		description := appkit.TextField_WrappingLabelWithString(*ptimer.Metadata.Description)
-		view.AddArrangedSubview(description)
+	scene := playerScene{
+		window: window,
+		ptimer: ptimer,
+		state:  waitingStart{},
 	}
-
-	padding := appkit.NewView()
-	view.AddArrangedSubview(padding)
-
-	// TODO: Implement player
-	next := appkit.NewButtonWithTitle("Start")
-	next.SetKeyEquivalent("\r")
-	next.SetControlSize(appkit.ControlSizeLarge)
-
-	actions := appkit.StackView_StackViewWithViews([]appkit.IView{next})
-	actions.SetOrientation(appkit.UserInterfaceLayoutOrientationHorizontal)
-	actions.SetAlignment(appkit.LayoutAttributeTrailing)
-	view.AddArrangedSubview(actions)
-
-	window.ContentView().AddSubview(view)
-	layout.PinEdgesToSuperView(view, foundation.EdgeInsets{Top: 8, Bottom: 8, Left: 16, Right: 16})
+	scene.Render()
 
 	window.MakeKeyAndOrderFront(nil)
 	window.Center()
 
-	return window
+	return &scene
 }
 
-func showPickerWindow(onPick func(ptimer *Ptimer)) appkit.Window {
+type pickerScene struct {
+	window appkit.Window
+}
+
+func (s *pickerScene) Terminate() {
+	s.window.Close()
+}
+
+func startPickerScene(onPick func(ptimer *Ptimer)) *pickerScene {
 	window := appkit.NewWindowWithSizeAndStyle(
 		150,
 		70,
@@ -153,7 +321,9 @@ func showPickerWindow(onPick func(ptimer *Ptimer)) appkit.Window {
 	window.MakeKeyAndOrderFront(window)
 	window.Center()
 
-	return window
+	return &pickerScene{
+		window: window,
+	}
 }
 
 type Asset struct {
